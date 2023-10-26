80ce90585306fd1079082c23d4df37e389b394c3
Beato <joaoluisbeato@outlook.com>
2023-10-26 08:14:25 -0300

    Update AspiradorDePo.py

---
+++ AspiradorDePo.py
@@ -0,0 +1,14 @@
+
+
+Salas = ['A','B','C','D','E','F','G','H','I','J']
+AspiradorDePo = ['','','','','','','','','','']
+Sujeira = ['','','','','','','','','','']
+
+ambiente = [Salas, AspiradorDePo, Sujeira]
+
+
+
+print("\n===========================\n")
+quantidade_salas = input(" Digite a quantidade de Salas (max = 10): ")
+quantidade_salas_sujas = input (" Digite quantas salas sujas você quer (max <= quantidade de salas): ")
+
