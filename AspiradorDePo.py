db223eebb040eb6708de820a7dd407f7bb3b240e
folasPuc <rafael.rsato@gmail.com>
2023-10-27 12:05:03 -0300

    preencher_sala
    preenche_sala com sujeira

--- AspiradorDePo.py
+++ AspiradorDePo.py
@@ -1,3 +1,20 @@
+import random
+
+def preencher_sala(quantidade_salas, quantidade_salas_sujas, ambiente):
+    if(quantidade_salas < quantidade_salas_sujas):
+        print("Quantidade maior de sujeiras do que salas")
+        return 0
+
+
+    while quantidade_salas_sujas != 0:
+        random_integer = random.randint(0, quantidade_salas - 1)
+        if(ambiente[2][random_integer] == ''):
+            ambiente[2][random_integer] = 'SUJEIRA'
+            quantidade_salas_sujas = quantidade_salas_sujas - 1
+        else:
+            continue
+    print(ambiente)
+    return ambiente
 
 
 Salas = ['A','B','C','D','E','F','G','H','I','J']
@@ -9,6 +26,8 @@ ambiente = [Salas, AspiradorDePo, Sujeira]
 
 
 print("\n===========================\n")
-quantidade_salas = input(" Digite a quantidade de Salas (max = 10): ")
-quantidade_salas_sujas = input (" Digite quantas salas sujas você quer (max <= quantidade de salas): ")
+quantidade_salas = int(input(" Digite a quantidade de Salas (max = 10): "))
+quantidade_salas_sujas = int(input (" Digite quantas salas sujas você quer (max <= quantidade de salas): "))
+
+preencher_sala(quantidade_salas, quantidade_salas_sujas, ambiente)
 
